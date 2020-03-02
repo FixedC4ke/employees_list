e15f3111a38fc744046228ca68055c928d834dfd
FixedC4ke <drmixe@gmail.com>
2020-03-02 20:35:42 +0500

    routes.rb improvment (get was removed)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'wposts/index'
-  get 'wposts/show'
-  get 'users/index'
-  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :wposts
